0cdd33bb3c9bc22a54c142ef647da34c0fad36ea
Dany Lemus <software.engineer.lemus@gmail.com>
2023-09-26 18:01:48 -0600

    Mejorando DTO y clase consulta para su buen funcionamiento

--- src/main/java/med/voll/api/dominio/consulta/validaciones/agendamiento/HorarioDeFuncionamientoClinica.java
+++ src/main/java/med/voll/api/dominio/consulta/validaciones/agendamiento/HorarioDeFuncionamientoClinica.java
@@ -14,7 +14,7 @@ public class HorarioDeFuncionamientoClinica implements ValidadorDeConsultas {
         var domingo = DayOfWeek.SUNDAY.equals(datos.fecha());
 
         var antesDeApertura = datos.fecha().getHour() < 7;
-        var despuesDeCierre = datos.fecha().getHour() < 19;
+        var despuesDeCierre = datos.fecha().getHour() > 19;
 
         if (domingo || antesDeApertura || despuesDeCierre) {
             throw new ValidationException("El horario de atencion es de lunes a sabado de 07:00 a 19:00 horas");
